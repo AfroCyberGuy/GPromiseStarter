9b23ff8a3b5a53100c2d119624cb6dc983a9ec69
tatendakabike <tkabike@gmail.com>
2019-12-05 19:33:35 +0200

    Initial Commit

---
+++ GPromiseStarter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GPromiseStarter
+//
+//  Created by Tatenda Kabike on 2019/12/05.
+//  Copyright © 2019 Tatenda Kabike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
